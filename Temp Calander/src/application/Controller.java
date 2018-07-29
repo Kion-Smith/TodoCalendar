02d1c36dd17516a66650ad1c4a37ae5ed592e5b6
Kion Smith <30888048+Kion-Smith@users.noreply.github.com>
2018-07-29 13:17:26 -0500

    Added functionality to month label

--- Temp Calander/src/application/Controller.java
+++ Temp Calander/src/application/Controller.java
@@ -1,15 +1,21 @@
 package application;
 
+import java.time.YearMonth;
+
 import javafx.event.ActionEvent;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.input.KeyEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
 
 
 public class Controller 
 {
+	
+	YearMonth curYearMonth = YearMonth.now();
+	
 	@FXML private VBox calanderVBox;
 	
 	//Buttons
@@ -18,19 +24,46 @@ public class Controller
 	@FXML private Button addBtn;
 	@FXML private Button RemoveBtn;
 	
+	//Labels
+	@FXML private Label curMonth;
+	@FXML private Label dateLabelsList[] = new Label[35];
+	
+	//Text Area
+	@FXML private TextArea InfoTextAreaList[] = new TextArea[35];
+	int temp =0;
+	
+	
+	
+	@FXML private void initialize()
+	{
+		curMonth.setText(curYearMonth.getMonth().toString()+" "+curYearMonth.getYear());
+	}
+	
 	//test button methods
+	
 	@FXML private void nextMonth(ActionEvent e)
 	{
+		
 		System.out.println("Next month pressed");
+		curYearMonth = curYearMonth.plusMonths(1);
+		curMonth.setText(curYearMonth.getMonth().toString()+" "+curYearMonth.getYear());
+		
+		
 	}
+	
 	@FXML private void lastMonth(ActionEvent e)
 	{
+		
 		System.out.println("Last month pressed");
+		curYearMonth = curYearMonth.minusMonths(1);
+		curMonth.setText(curYearMonth.getMonth().toString()+" "+curYearMonth.getYear());
 	}
+	
 	@FXML private void assignNote(ActionEvent e)
 	{
 		System.out.println("Sent note");
 	}
+	
 	@FXML private void deleteNote(ActionEvent e)
 	{
 		System.out.println("Clear Note");
