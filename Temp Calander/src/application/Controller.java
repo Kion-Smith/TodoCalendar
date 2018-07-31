7b4f6046566fbcc85a910dd5b39034ae88faaed0
Kion Smith <30888048+Kion-Smith@users.noreply.github.com>
2018-07-31 13:12:48 -0500

    Fixed mistake label array;working on adding days

--- Temp Calander/src/application/Controller.java
+++ Temp Calander/src/application/Controller.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.time.LocalDate;
 import java.time.YearMonth;
 
 import javafx.event.ActionEvent;
@@ -17,6 +18,7 @@ public class Controller
 {
 	
 	YearMonth curYearMonth = YearMonth.now();
+	
 	
 	@FXML private VBox calanderVBox;
 	@FXML private GridPane calanderPane;
@@ -46,30 +48,90 @@ public class Controller
 		//Adding labels and Text area to the boxes using borderPane
 		//this is temporary 
 		int count =1;
+	
 		
 		for(int r=0;r<5;r++)
 		{
 			for(int c = 0;c<7;c++)
 			{
-				borderPaneList[r+c] = new BorderPane();
-				dateLabelsList[r+c] = new Label(" L "+ count );
-				InfoTextAreaList[r+c] = new TextArea();
+				int curLoc = r*7+c;
+				borderPaneList[curLoc] = new BorderPane();
+				dateLabelsList[curLoc] = new Label(" L "+ count );
+				InfoTextAreaList[curLoc] = new TextArea();
 				
 				//Unsure about this ->  //InfoTextAreaList[r+c].setEditable(false);
 				
-				InfoTextAreaList[r+c].setMaxSize(110, 83);
-				
-				borderPaneList[r+c].setTop(dateLabelsList[r+c]);
-				borderPaneList[r+c].setCenter(InfoTextAreaList[r+c]);
-				calanderPane.add(borderPaneList[r+c],c,r);
+				InfoTextAreaList[r*7+c].setMaxSize(110, 83);			
+				borderPaneList[r*7+c].setTop(dateLabelsList[curLoc]);
+				borderPaneList[r*7+c].setCenter(InfoTextAreaList[curLoc]);
+				calanderPane.add(borderPaneList[curLoc],c,r);
 
 				count++;
 			}
 		}
 		
+		
+		setCalanderDates();
 	}
 	
 	//test button methods
+	
+	@FXML private void setCalanderDates()
+	{
+		LocalDate startDate = LocalDate.of(curYearMonth.getYear(),curYearMonth.getMonth(),1);
+		LocalDate endDate = LocalDate.of(curYearMonth.getYear(),curYearMonth.getMonth(), startDate.withDayOfMonth(startDate.lengthOfMonth()).getDayOfMonth() );
+		/*
+	     while (!curDay.getDayOfWeek().toString().equals("SUNDAY") ) {
+	    	 curDay = curDay.minusDays(1);
+	        }
+		*/
+		System.out.println("START :"+startDate+" AKA: "+startDate.getDayOfWeek().toString());
+		System.out.println("END :"+endDate+" AKA: "+endDate.getDayOfWeek().toString());
+		
+		
+		for(int r=0;r<5;r++)
+		{
+			for(int c = 0;c<7;c++)
+			{	
+				int curLoc = r*7+c;
+				//dateLabelsList[curLoc].setText("MEME");
+
+				
+			}
+		}
+
+	}
+	
+	private int DayStringToNums(String day)
+	{
+		int numvalue = -1;
+		
+		switch(day)
+		{
+			case "SUNDAY":
+				numvalue = 0;
+				break;
+			case "MONDAY":
+				numvalue = 1;
+				break;
+			case "TUESDAY":
+				numvalue = 2;
+				break;
+			case "WEDNESDAY":
+				numvalue = 3;
+				break;
+			case "THURSDAY":
+				numvalue = 4;
+				break;
+			case "FRIDAY":
+				numvalue = 5;
+				break;
+			case "SATURDAY":
+				numvalue = 6;
+				break;
+		}
+		return numvalue;
+	}
 	
 	@FXML private void nextMonth(ActionEvent e)
 	{
@@ -78,7 +140,7 @@ public class Controller
 		curYearMonth = curYearMonth.plusMonths(1);
 		curMonth.setText(curYearMonth.getMonth().toString()+" "+curYearMonth.getYear());
 		
-		
+		setCalanderDates();
 	}
 	
 	@FXML private void lastMonth(ActionEvent e)
@@ -87,6 +149,8 @@ public class Controller
 		System.out.println("Last month pressed");
 		curYearMonth = curYearMonth.minusMonths(1);
 		curMonth.setText(curYearMonth.getMonth().toString()+" "+curYearMonth.getYear());
+		
+		setCalanderDates();
 	}
 	
 	@FXML private void assignNote(ActionEvent e)
