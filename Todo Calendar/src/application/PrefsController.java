e830faa00ba0792ea0fdb9976ee4e8c7141c4718
Kion Smith <30888048+Kion-Smith@users.noreply.github.com>
2018-08-08 14:08:20 -0500

    +another pane with fxml to create settings menu

--- Todo Calendar/src/application/PrefsController.java
+++ Todo Calendar/src/application/PrefsController.java
@@ -3,9 +3,14 @@ package application;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 
 public class PrefsController
 {
+	@FXML BorderPane SettingsPane;
+	@FXML Pane sP;
+	
 	@FXML Button applyChangesBtn;
 	@FXML Button resetDefaultBtn;
 	
@@ -14,5 +19,22 @@ public class PrefsController
 	@FXML private void initialize()
 	{
 	
+	}
+	@FXML private void displayMenu()
+	{
+		
+		if(SettingsList.getSelectionModel().getSelectedIndex() ==0)
+		{
+			GeneralSettings gs = new  GeneralSettings();
+			sP.getChildren().add(gs);
+			
+			//SettingsPane.setCenter(gs);
+			//SettingsPane;
+			
+		}
+		
+		
+		//System.out.println(SettingsList.getSelectionModel().getSelectedIndex());
+		
 	}
 }
