5ab422700dbe3cf5dad8619a85687d8cea0cdf8f
Kion-Smith <kion_smith@hotmail.com>
2019-02-26 00:09:11 -0600

    basic setup

---
+++ [New Version]/Desktop App/Todo Calendar Desktop App [Revised]/src/main/main.java
@@ -0,0 +1,26 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class main extends Application 
+{
+	
+	public  static void main(String args[])
+	{
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage arg0) throws Exception 
+	{
+		//add the jaavafx stuff here
+	}
+	
+	@Override 
+	public void stop()
+	{
+		System.exit(0);
+	}
+
+}
