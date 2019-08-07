9792d760490585bf7f93c21f3318071e39062b59
Kion-Smith <kion_smith@hotmail.com>
2019-08-06 20:42:10 -0500

    trying to fix things

--- [New Version]/Desktop App/Todo Calendar Desktop App [Revised]/src/main/mainController.java
+++ [New Version]/Desktop App/Todo Calendar Desktop App [Revised]/src/main/mainController.java
@@ -1,6 +1,35 @@
 package main;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.GridPane;
+
 public class mainController 
 {
+
+    @FXML
+    private MenuBar menuBar;
+
+    @FXML
+    private GridPane calendarGrid;
+
+    @FXML
+    private Label monthYearLabel;
+
+    @FXML
+    private Button leftArrowButton;
+
+    @FXML
+    private Button rightArrowButton;
+    
+    /*
+    @FXML
+    void handleButtonClick(ActionEvent event) 
+    {
+    	System.out.println("ERA");
+    }*/
 
 }
